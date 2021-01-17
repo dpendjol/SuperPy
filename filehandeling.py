c56638781b86d4bf30af665779e1960111f62c58
Dominique Pendjol <d.pendjol@gmail.com>
2021-01-17 20:31:29 +0100

    got the cost calculated

--- filehandeling.py
+++ filehandeling.py
@@ -3,18 +3,22 @@ from datetime import date
 
 def main():
     file_content = read_file('./sold.csv')
-    revenue = get_revenue(file_content, date.fromisoformat('2021-01-03'), 'month')
-    print(revenue)
-    
+    selection = get_selection(file_content, date.fromisoformat('2021-01-03'), 'month')
+    revenue = get_numbers(selection)
+    print(revenue)    
 
 
 def read_file(file_name) -> list:
+    '''
+    Reads the csv file and exports its in a dict.
+    Each line becomes a dict in a list
+    '''
     with open(file_name, newline='') as f:
         reader = csv.DictReader(f)
         return (list(reader))
 
 
-def get_revenue(data:list, filter_date:date, period:str='day') -> float:
+def get_selection(data:list, filter_date:date, period:str='day') -> float:
     '''
     Get the rows of data within a specifiec date
     @param data list of dicts with data
@@ -23,8 +27,6 @@ def get_revenue(data:list, filter_date:date, period:str='day') -> float:
     @returns a float containing the revenue of the day or a month
     '''
     found_rows = []
-    total_revenue = 0
-    total_costs = 0
     
     if period == 'month':
         myfilter = filter_date.isoformat()[0:7]
@@ -36,16 +38,19 @@ def get_revenue(data:list, filter_date:date, period:str='day') -> float:
     for item in data:
         if item['sell_date'][0:end] == myfilter:
             found_rows.append(item)
-            total_costs += get_costs(read_file('bought.csv'), 
-                                     item['bought_id'],
-                                     item['count'])
-            total_revenue += int(item['count']) * float(item['sell_price'])
-    
-    print(total_revenue)
-    print(total_costs)
-    print('Profit: ', total_revenue - total_costs)
-    return total_revenue
+    return found_rows
             
+
+def get_numbers(data:list):
+    total_revenue = 0
+    total_costs = 0
+    for item in data:
+        total_costs += get_costs(read_file('bought.csv'), 
+                                item['bought_id'],
+                                item['count'])
+        total_revenue += int(item['count']) * float(item['sell_price'])
+    return {'revenue': total_revenue, 'costs': total_costs}
+
 def get_costs(data:list, product_id:str, number_sold:int):
     for item in data:
         if item['id'] == product_id:
