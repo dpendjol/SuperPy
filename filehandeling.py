6ee2614a008ec1c87873719af8768f2f0471c9cf
Dominique Pendjol <d.pendjol@gmail.com>
2021-01-17 17:00:51 +0100

    working on costs [NOT DONE]

--- filehandeling.py
+++ filehandeling.py
@@ -3,7 +3,7 @@ from datetime import date
 
 def main():
     file_content = read_file('./sold.csv')
-    revenue = get_revenue(file_content, date.fromisoformat('2021-02-03'), 'month')
+    revenue = get_revenue(file_content, date.fromisoformat('2021-01-03'), 'month')
     print(revenue)
     
 
@@ -24,6 +24,7 @@ def get_revenue(data:list, filter_date:date, period:str='day') -> float:
     '''
     found_rows = []
     total_revenue = 0
+    total_costs = 0
     
     if period == 'month':
         myfilter = filter_date.isoformat()[0:7]
@@ -35,11 +36,22 @@ def get_revenue(data:list, filter_date:date, period:str='day') -> float:
     for item in data:
         if item['sell_date'][0:end] == myfilter:
             found_rows.append(item)
-            
+            total_costs += get_costs(read_file('bought.csv'), 
+                                     item['bought_id'],
+                                     item['count'])
             total_revenue += int(item['count']) * float(item['sell_price'])
     
+    print(total_revenue)
+    print(total_costs)
+    print('Profit: ', total_revenue - total_costs)
     return total_revenue
             
-
+def get_costs(data:list, product_id:str, number_sold:int):
+    for item in data:
+        if item['id'] == product_id:
+            costs = int(item['count']) * float(item['price'])
+            print(f'Costs of {item["product_name"]}: ', costs)
+    return costs            
+    
 if __name__ == "__main__":
     main()
